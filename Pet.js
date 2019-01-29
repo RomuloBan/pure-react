25f90062a8b8f93e3aeb68d8a8e58d93a5df7f5b
romulo.banares <romulo88@hotmail.com>
2019-01-29 23:47:21 +0100

    routing with reach router link

--- Pet.js
+++ Pet.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "@reach/router";
 class Pet extends React.Component {
   render() {
-    const { animal, name, breed, media, location } = this.props;
+    const { animal, name, breed, media, location, id } = this.props;
 
     let photos = [];
 
@@ -9,7 +10,7 @@ class Pet extends React.Component {
       photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
     }
     return (
-      <div className="pet">
+      <Link to={`/details/${id}`} className="pet">
         <div className="image-container">
           <img src={photos[0].value} alt={name} />
         </div>
@@ -19,7 +20,7 @@ class Pet extends React.Component {
             {animal} - {breed} - {location}
           </h2>
         </div>
-      </div>
+      </Link>
     );
   }
 }
